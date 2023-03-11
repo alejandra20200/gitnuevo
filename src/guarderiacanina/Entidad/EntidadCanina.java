dee641f7e86efcd04af938443380cf1c5930abf7
Alejandra Aguirre <alaguirre@gmail.com>
2023-03-11 15:16:26 -0300

    se agregan construtores

--- src/guarderiacanina/Entidad/EntidadCanina.java
+++ src/guarderiacanina/Entidad/EntidadCanina.java
@@ -13,5 +13,37 @@ public class EntidadCanina {
     private String nombre;
    private int edad;
    private String dueño;
+
+    public EntidadCanina(String nombre, int edad, String dueño) {
+        this.nombre = nombre;
+        this.edad = edad;
+        this.dueño = dueño;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public int getEdad() {
+        return edad;
+    }
+
+    public void setEdad(int edad) {
+        this.edad = edad;
+    }
+
+    public String getDueño() {
+        return dueño;
+    }
+
+    public void setDueño(String dueño) {
+        this.dueño = dueño;
+    }
+
+   
     
 }
